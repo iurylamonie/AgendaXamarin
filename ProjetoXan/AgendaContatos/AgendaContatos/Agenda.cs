ec8e4ac42060dbc6b1b9a0b684689f2eda18be0f
Iury Lamonie <iury.lamonie@hotmail.com>
2016-12-02 14:20:45 -0300

    Update Agenda.cs

--- ProjetoXan/AgendaContatos/AgendaContatos/Agenda.cs
+++ ProjetoXan/AgendaContatos/AgendaContatos/Agenda.cs
@@ -10,14 +10,15 @@ namespace AgendaContatos
     
     public class Agenda : TabbedPage
     {
-        
+        //Lista de Contatos
         ListView listView = new ListView();
+        Button atualizarLista = new Button { Text = "Atualizar Lista", HorizontalOptions = LayoutOptions.Center };
 
-
+        //Adicionar contato
         Entry nomeUsuario = new Entry { Placeholder = "Nome"};
         Entry foneUsuario = new Entry { Placeholder = "Telefone"};
         Button adicionarUsuario = new Button { Text = "Adicionar", HorizontalOptions = LayoutOptions.Center };
-        Button atualizarLista = new Button { Text = "Atualizar Lista", HorizontalOptions = LayoutOptions.Center };
+       
 
         List<Models.Contato> contatos = new List<Models.Contato>();
 
@@ -49,7 +50,6 @@ namespace AgendaContatos
         private void AtualizarLista_Clicked(object sender, EventArgs e)
         {
             listView.ItemsSource = contatos;
-            new Agenda();
         }
 
         private void AdicionarUsuario_Clicked(object sender, EventArgs e)
